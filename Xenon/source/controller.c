609c490f482c235cf2785a235fb17379f90c5a23
Local Chicken <localcmail@gmail.com>
2019-11-25 19:53:17 +0200

    reworked lwip server, and made it actually send data!

--- Xenon/source/controller.c
+++ Xenon/source/controller.c
@@ -1,40 +1,30 @@
 #include "controller.h"
+#include <usb/usbmain.h>
+#include <stdio.h>
+#include <string.h>
 
-
-bool compare_controller_data(struct controller_data_s f, struct controller_data_s s) {
-	return f.s1_x == s.s1_x && f.s1_y == s.s1_y && 
-	       f.s2_x == s.s2_x && f.s2_y == s.s2_y &&
-	       f.s1_z == s.s1_z && f.s2_z == f.s2_z &&
-	       f.lb == s.lb && f.rb == s.rb && 
-	       f.start == s.start && f.back == s.back &&
-	       f.a == s.a && f.b == s.b && f.x == s.x && f.y == s.y &&
-	       f.up == s.up && f.down == s.down && f.left == s.left && f.right == s.right &&
-	       f.rt == s.rt && f.lt == s.lt &&
-	       f.logo == s.logo;
-}
 
 void serialize_controller_data(struct controller_data_s controller_data, unsigned char* data_to_send) {
 	//Assuming 14 bytes were allocated
 	//Zeroing out
-	for(char i = 0; i < 14; i++){
-		data_to_send[i] = 0;
-	}
-	data_to_send[0] |= ((controller_data.a << GAMEPAD_OFFSET_A) |
-			    (controller_data.x << GAMEPAD_OFFSET_X) |
-			    (controller_data.b << GAMEPAD_OFFSET_B) |
-			    (controller_data.y << GAMEPAD_OFFSET_Y) |
-			    (controller_data.lb << GAMEPAD_OFFSET_LB) |
-			    (controller_data.rb << GAMEPAD_OFFSET_RB) |
-			    (controller_data.up << GAMEPAD_OFFSET_UP) |
-			    (controller_data.down << GAMEPAD_OFFSET_DOWN));
-	data_to_send[1] |= ((controller_data.left << GAMEPAD_OFFSET_LEFT) |
-			    (controller_data.right << GAMEPAD_OFFSET_RIGHT) |
-			    (controller_data.s1_z << GAMEPAD_OFFSET_S1_Z) |
-			    (controller_data.s2_z << GAMEPAD_OFFSET_S2_Z) |
-			    (controller_data.start << GAMEPAD_OFFSET_START) |
-			    (controller_data.back << GAMEPAD_OFFSET_BACK) |
-			    (controller_data.logo << GAMEPAD_OFFSET_LOGO) |
-			    (0 << 8));
+	memset(data_to_send, 0, 14);
+	printf("button a: %d\n", controller_data.a);
+	data_to_send[0] = ((controller_data.a) |
+			    (controller_data.x << GB_OFF_X) |
+			    (controller_data.b << GB_OFF_B) |
+			    (controller_data.y << GB_OFF_Y) |
+			    (controller_data.lb << GB_OFF_LB) |
+			    (controller_data.rb << GB_OFF_RB) |
+			    (controller_data.up << GB_OFF_UP) |
+			    (controller_data.down << GB_OFF_DOWN));
+	data_to_send[1] = ((controller_data.left) |
+			    (controller_data.right << GB_OFF_RIGHT) |
+			    (controller_data.s1_z << GB_OFF_S1_Z) |
+			    (controller_data.s2_z << GB_OFF_S2_Z) |
+			    (controller_data.start << GB_OFF_START) |
+			    (controller_data.back << GB_OFF_BACK) |
+			    (controller_data.logo << GB_OFF_LOGO) |
+			    (0 << 7));
 	//Triggers
 	data_to_send[2] = controller_data.lt & 0xFF;
 	data_to_send[3] = controller_data.lt >> 8;
@@ -58,11 +48,9 @@ void read_controller_data(unsigned char* data_to_send) {
 	//Assuming data_to_send was allocated 14 bytes long
 	struct controller_data_s cur_controller_data;
 	if(get_controller_data(&cur_controller_data, 0) > -1) {
-		if(!compare_controller_data(cur_controller_data, prev_controller_data)) {
-			serialize_controller_data(cur_controller_data, data_to_send);
-			prev_controller_data = cur_controller_data;
-			
-		}
-		usb_do_poll();
+		serialize_controller_data(cur_controller_data, data_to_send);
+		prev_controller_data = cur_controller_data;
+
 	}
+	usb_do_poll();
 }
--- Xenon/source/server.c
+++ Xenon/source/server.c
@@ -1,174 +1,72 @@
 #include "server.h"
-
-struct tcp_pcb* listen_pcb;
-
-
-
-void server_send(struct tcp_pcb* pcb, struct server_state* state){
-	struct pbuf* ptr;
-	err_t r_err = ERR_OK;
-	while((r_err == ERR_OK) && (state->p != NULL) && (state->p->len <= tcp_sndbuf(pcb))){
-		ptr = state->p;
-
-		r_err = tcp_write(pcb, ptr->payload, ptr->len, 1);
-		if(r_err == ERR_OK){
-			//Data sending
-			u16_t plen;
-			u8_t freed;
-
-			plen = ptr->len;
-			state->p = ptr->next;
-			if(state->p != NULL){
-				pbuf_ref(state->p);
-			}
-
-			do {
-				freed = pbuf_free(ptr);
-			}while(freed == 0);
-
-			tcp_recved(pcb, plen);
-		}else if(r_err == ERR_MEM) {
-			state->p = ptr;
-		}
-	}
-}
-
+#include "controller.h"
 
 void conn_err(void *arg, err_t err) {
-	printf("err\n");
+	printf("error!\n");
 }
 
-void close_conn(struct tcp_pcb *pcb, struct server_state* state) {
+void close_conn(struct tcp_pcb *pcb) {
 	tcp_arg(pcb, NULL);
 	tcp_sent(pcb, NULL);
 	tcp_recv(pcb, NULL);
-	tcp_err(pcb, NULL);
 	tcp_poll(pcb, NULL, 0);
-	if(state != NULL) {
-		free(state);
-	}
+	tcp_err(pcb, NULL);
 	tcp_close(pcb);
 }
 
 
-err_t server_poll(void *arg, struct tcp_pcb *pcb) {	
-	struct server_state* state;
-
-	state = (struct server_state*)arg;
-	if(state != NULL){
-		if(state->p != NULL){
-			tcp_sent(pcb, server_sent);
-			server_send(pcb, state);
-		}
-	}
-
-	printf("polling!\n");
-
-	return ERR_OK;
-}
-
-err_t server_sent(void *arg, struct tcp_pcb *pcb, u16_t len) {
-	struct server_state* state;
-
-
-	LWIP_UNUSED_ARG(len);
-
-	state = (struct server_state*)arg;
-
-	if(state->p != NULL){
-		tcp_sent(pcb, server_sent);
-		server_send(pcb, state);
-	}
-	return ERR_OK;
+void reply_controller_data(struct tcp_pcb* pcb) {
+	//TODO: actually reply something!
+	unsigned char* buffer = (unsigned char*)malloc(14); // 14 bytes for controller data, see controller.c for more info
+	read_controller_data(buffer);
+	tcp_write(pcb, buffer, 14, 0);
+	tcp_sent(pcb, NULL); // no cb
 }
 
 err_t server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err) {
 
-	struct server_state* state;
-	err_t r_err;
+	if(err == ERR_OK && p != NULL){
+		//Inform about data being taken
+		tcp_recved(pcb, p->tot_len);
+		unsigned char command = ((unsigned char*)p->payload)[0];
+		
+		if(command == 1) {
+			reply_controller_data(pcb);
+		}else if(command == 2) {
+			close_conn(pcb);
+		}
+		pbuf_free(p);
 
-	state = (struct server_state*)arg;
-	if(p == NULL) {
-		//Conn closed
-		if(state->p == NULL){
-			//Done
-			close_conn(pcb, state);
-		}else{
-			tcp_sent(pcb, server_sent);
-			server_send(pcb, state);
-		}
-		r_err = ERR_OK;
-	}else if(err != ERR_OK){
-		if(p != NULL) {
-			state->p = NULL;
-			pbuf_free(p);
-		}
-		r_err = err;
+		tcp_sent(pcb, NULL);
+
+	}else if(err == ERR_OK && p == NULL){
+		close_conn(pcb);
 	}else {
-		if(state->p == NULL){
-			state->p = p;
-
-			tcp_sent(pcb, server_sent);
-			//Send controller data
-		}else {
-			struct pbuf *buf;
-			buf = state->p;
-			pbuf_chain(buf, p);
-		}
-
-		r_err = ERR_OK;
-
+		return err;
 	}
+	return ERR_OK;
 
-
-	return r_err;
 }
 
-err_t server_accept(void *arg, struct tcp_pcb *pcb, err_t err) {
-	struct server_state* state;
-	err_t r_err;
-
+err_t server_accept(void *arg, struct tcp_pcb *pcb, err_t err) {		
 	LWIP_UNUSED_ARG(arg);
 	LWIP_UNUSED_ARG(err);
-
 	tcp_setprio(pcb, TCP_PRIO_MIN);
-	
-	state = (struct server_state*)malloc(sizeof(struct server_state));
-	if(state != NULL) {
-		state->pcb = pcb;
-		state->p = NULL;
 
-		tcp_arg(pcb, state);
-
-		tcp_recv(pcb, server_recv);
-
-		tcp_err(pcb, conn_err);
-		tcp_sent(pcb, server_sent);
-		
-		tcp_poll(pcb, server_poll, 4);
-		
-		tcp_accepted(listen_pcb); 
-		r_err = ERR_OK;
-	}else {
-		r_err = ERR_MEM;
-	}
-
-	printf("accept!\n");
-	return r_err;
+	tcp_arg(pcb, NULL);
+	tcp_recv(pcb, server_recv);
+	tcp_err(pcb, conn_err);
+	tcp_poll(pcb, NULL, 0); // no polling
+	return ERR_OK;
 }
 
 void start_server(void) {
-	//struct tcp_pcb *pcb;
 
 	listen_pcb = tcp_new();
-	if(listen_pcb != NULL) {
-		err_t err;
-		err = tcp_bind(listen_pcb, IP_ADDR_ANY, 1182);
-		if(err == ERR_OK) {		
-			listen_pcb = tcp_listen(listen_pcb);
-			tcp_accept(listen_pcb, server_accept);
-		}
-	}
+	tcp_bind(listen_pcb, IP_ADDR_ANY, 1182);
+
+	listen_pcb = tcp_listen(listen_pcb);
+	tcp_accept(listen_pcb, server_accept);
 }
 
 
